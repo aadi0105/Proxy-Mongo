94f2a66405657cddbd25d17dd1fc40a00064886e
EC2 Default User <ec2-user@ip-172-31-42-86.us-east-2.compute.internal>
2022-08-25 20:36:59 +0000

    changed db name

--- mongo.py
+++ mongo.py
@@ -4,7 +4,7 @@ if __name__=='__main__':
     print("welocme to pymongo")
     client = pymongo.MongoClient("mongodb://ip-172-31-46-205.us-east-2.compute.internal:27017/")
     print(client)
-    db = client['harry']
+    db = client['marks']
     collection = db['abc']
     dictionary = {'name': 'aadi', 'marks':50}
     collection.insert_one(dictionary)
